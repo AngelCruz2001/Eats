563f3490255aec99055e71da15078636bddf85b1
AngelCruz2001 <34138352+AngelCruz2001@users.noreply.github.com>
2020-04-15 10:25:21 -0500

    Add time

--- app/src/main/java/com/jpg/MainActivity.java
+++ app/src/main/java/com/jpg/MainActivity.java
@@ -3,14 +3,24 @@ package com.jpg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Handler mHandler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login2);
 
+        mHandler.postDelayed(openNew, 1500);
 
     }
+    private Runnable openNew = new Runnable() {
+        @Override
+        public void run() {
+
+        }CAAALL MEEEEEEE
+    }
+
 }
