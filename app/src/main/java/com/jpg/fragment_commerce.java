4bf1bc5130973cf557b2905cbbb031f1ebf1ddcc
AngelCruz2001 <34138352+AngelCruz2001@users.noreply.github.com>
2020-04-16 06:25:03 -0500

    almost again
    We can do it!
    
    Co-Authored-By: Angel Retana <aerm2018@users.noreply.github.com>

--- app/src/main/java/com/jpg/fragment_commerce.java
+++ app/src/main/java/com/jpg/fragment_commerce.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -58,7 +60,12 @@ public class fragment_commerce extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_commerce, container, false);
+        View v = inflater.inflate(R.layout.fragment_commerce, container, false);
+        Spinner spinner = (Spinner) v.findViewById(R.id.food);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),R.array.food_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        return v;
+
     }
 }
