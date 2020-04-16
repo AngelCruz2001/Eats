99de3692ea03e49a681cd43dbf8d6c8a2d554fba
AngelCruz2001 <34138352+AngelCruz2001@users.noreply.github.com>
2020-04-16 03:45:14 -0500

    Update Fragments added
    Co-Authored-By: Angel Retana <aerm2018@users.noreply.github.com>

--- app/src/main/java/com/jpg/RegistroActivity.java
+++ app/src/main/java/com/jpg/RegistroActivity.java
@@ -1,14 +1,20 @@
 package com.jpg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class RegistroActivity extends AppCompatActivity {
-
+    universal universal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        universal = new universal();
+        FragmentTransaction ft =  getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.fragments,universal);
+        ft.commit();
     }
 }
