5f9ac24799fe48387ea70d557e50e6c19222d420
AngelCruz2001 <34138352+AngelCruz2001@users.noreply.github.com>
2020-04-16 13:17:46 -0500

    Presentable
    We did it.
    
    Co-Authored-By: Angel Retana <aerm2018@users.noreply.github.com>

--- app/src/main/java/com/jpg/RegistroActivity.java
+++ app/src/main/java/com/jpg/RegistroActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,8 +15,11 @@ public class RegistroActivity extends AppCompatActivity {
     fragment_client fragment_client;
     fragment_commerce fragment_commerce;
     fragment_deliver fragment_deliver;
+    animationBWATB animation;
     TextView btnNext;
     String type;
+    private Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,27 +34,17 @@ public class RegistroActivity extends AppCompatActivity {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String prueba = getVariable();
-                FragmentTransaction ft =  getSupportFragmentManager().beginTransaction();
-                switch (prueba){
-                    case "commerce":
-                        fragment_commerce = new fragment_commerce();
-                        ft.replace(R.id.fragments, fragment_commerce);
-                        ft.addToBackStack(null);
-                        ft.commit();
-                        break;
-                    case "client":
-                        Intent intent = new Intent(RegistroActivity.this,userMenu.class);
-                        startActivityForResult(intent, 0);
-                        break;
-                    case "deliver":
-                        fragment_deliver = new fragment_deliver();
-                        ft.replace(R.id.fragments, fragment_deliver);
-                        ft.addToBackStack(null);
-                        ft.commit();
-                        break;
-                }
-                Toast.makeText(getApplicationContext(),prueba,Toast.LENGTH_LONG);
+                final String prueba = getVariable();
+                animation = new animationBWATB();
+                final FragmentTransaction ft =  getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.fragments, animation);
+                ft.commit();
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        change(prueba);
+                    }
+                },1500);
             }
         });
     }
@@ -59,5 +53,29 @@ public class RegistroActivity extends AppCompatActivity {
     }
     public String getVariable(){
         return type;
+    }
+    public void change(String prueba){
+        FragmentTransaction ft =  getSupportFragmentManager().beginTransaction();
+
+        switch (prueba){
+            case "commerce":
+                fragment_commerce = new fragment_commerce();
+                ft =  getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.fragments, fragment_commerce);
+                ft.addToBackStack(null);
+                ft.commit();
+                break;
+            case "client":
+                Intent intent = new Intent(RegistroActivity.this,userMenu.class);
+                startActivityForResult(intent, 0);
+                break;
+            case "deliver":
+                fragment_deliver = new fragment_deliver();
+                ft =  getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.fragments, fragment_deliver);
+                ft.addToBackStack(null);
+                ft.commit();
+                break;
+        }
     }
 }
